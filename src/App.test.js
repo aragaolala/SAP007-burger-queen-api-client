6155db34851e3a87bff171164d1c5cbb12758c6d
Layssa <oilayssaa@gmail.com>
2022-06-14 14:32:41 -0300

    teste input junto com seu componente

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,5 @@
-import { render } from '@testing-library/react';
 import LoginUser from './pages/login/login';
 import Register from './pages/register/Register';
-// import Button from './components/button/Button';
-import Input from './components/input/Input';
 
 /* eslint-disable */
 // testes login e registro
@@ -15,20 +12,6 @@ describe("Login", () => {
 describe("Register", () => {
   it("It should be a function", () => {
     expect(typeof Register).toBe("function");
-  });
-});
-
-
-
-// testes componente input
-describe("Testing input component", () => {
-  it("should render the input component", () => {
-    render(<Input />);
-  });
-
-  it("should call the onChange function when value is changed", () => {
-    const onChange = jest.fn();
-    render(<Input onChange={onChange} />);
   });
 });
 
